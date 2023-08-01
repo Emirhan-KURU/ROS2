022bb511193c020cbc49748648d02b6d108598a1
Emirhan-KURU <2kuru7emirhan1@gmail.com>
2023-08-01 16:00:52 +0300

    first commit

---
+++ src/my_py_pkg/my_py_pkg/deneme_20.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+import rclpy 
+from rclpy.node import Node
+
+def main(args = None):
+    rclpy.init(args=args)
+    node = Node("Video_20_deneme")
+    node.get_logger().info("Deneme_20_Basarili")
+    rclpy.shutdown()
+
+
+def say_to_10():
+    for i in range(1, 11):
+        print(i)
+
+if __name__ == "__main__":
+    main()
+    say_to_10()
+
+    
+
